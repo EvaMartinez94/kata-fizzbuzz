18d9ddad31a7eba856f9a4381b9b4cb44f767344
EvaMartinez94 <evamartinezlopez94@hotmail.com>
2024-06-24 19:35:47 +0200

    kata fizzbuzz

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,20 @@
+package org.example;
+
+public class Main {
+  public static void main(String[] args) {
+    for (int i = 1; i <= 100; i++) {
+     System.out.println(execute(i));
+    }
+  }
+  public static String execute(int i){
+      if (i % 3 == 0 && i % 5 == 0) {
+        return "FizzBuzz";
+      } else if (i % 5 == 0) {
+        return "Buzz";
+      } else if (i % 3 == 0) {
+        return "Fizz";
+      } else {
+       return String.valueOf(i);
+      }
+    }
+  }
---
+++ src/test/java/FizzBuzzTest.java
@@ -0,0 +1,36 @@
+import org.example.Main;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class FizzBuzzTest {
+
+  @Test
+  void fizzBuzz_divisible_by_3_and_5() {
+
+    String divisible = Main.execute(15);
+
+    assertEquals("FizzBuzz", divisible);
+  }
+  @Test
+  void fizzBuzz_divisible_by_3() {
+
+    String divisible = Main.execute(9);
+
+    assertEquals("Fizz", divisible);
+  }
+  @Test
+  void fizzBuzz_divisible_by_5() {
+
+    String divisible = Main.execute(50);
+
+    assertEquals("Buzz", divisible);
+  }
+  @Test
+  void fizzBuzz_no_divisible() {
+
+    String divisible = Main.execute(7);
+
+    assertEquals("7", divisible);
+  }
+
+}
